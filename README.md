e4b943e9e156eb843a41dd5e741a6f4aee5b6952
Amin56413 <Ellie.guo@goldenf.co>
2021-04-12 18:31:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WMSlot interface
---
+++ src/Contract/WMSlotServiceInterface.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\WMSlot\Contract;
+/**
+ * 博樂棋牌
+ */
+interface WMSlotServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @param string|null $back_url
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $back_url = null);
+
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id);
+
+    /**
+     * 抓取遊戲紀錄
+     *
+     * @param string $op_code
+     * @param integer $past_minutes
+     * @param string $cache_key
+     * @return mixed
+     */
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+
+    /**
+     * 全營商 抓取遊戲紀錄
+     *
+     * @param integer $past_minutes
+     * @return mixed
+     */
+    function betLogGrabberAll(int $past_minutes);
+}
+
